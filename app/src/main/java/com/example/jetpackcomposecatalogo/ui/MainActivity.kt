70ad4efc028aeb1d4d427896b638e7c959160f85
“soniardza” <sonia.rdzalv@gmail.com>
2023-06-13 12:02:32 -0600

    My first example with States

--- app/src/main/java/com/example/jetpackcomposecatalogo/ui/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/ui/MainActivity.kt
@@ -17,10 +17,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,10 +41,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyStateExample() {
+    var counter = remember {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter.value += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado ${counter.value} veces")
     }
 }
 
@@ -183,6 +204,7 @@ fun DefaultPreview() {
 //        MyBox()
 //        MyColumn()
 //        MyRow()
-        MyComplexLayout()
+//        MyComplexLayout()
+        MyStateExample()
     }
 }
