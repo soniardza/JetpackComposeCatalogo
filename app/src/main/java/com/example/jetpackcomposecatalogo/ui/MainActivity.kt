c5de7b006cc295010ef95dfe50247fc2560f3a0b
“soniardza” <sonia.rdzalv@gmail.com>
2023-07-20 10:57:13 -0600

    Learning text attributes

--- app/src/main/java/com/example/jetpackcomposecatalogo/ui/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/ui/MainActivity.kt
@@ -29,6 +29,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposecatalogo.ui.theme.JetpackComposeCatalogoTheme
@@ -43,10 +47,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyStateExample()
+                    MyText()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(
+                        TextDecoration.Underline,
+                        TextDecoration.LineThrough
+                    )
+                )
+            )
+        )
     }
 }
 
@@ -207,6 +241,7 @@ fun DefaultPreview() {
 //        MyColumn()
 //        MyRow()
 //        MyComplexLayout()
-        MyStateExample()
+//        MyStateExample()
+        MyText()
     }
 }
