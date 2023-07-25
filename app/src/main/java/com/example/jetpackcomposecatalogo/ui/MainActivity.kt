2f83a3d2906214e0d50592b237b5375965706864
“soniardza” <sonia.rdzalv@gmail.com>
2023-07-25 08:58:24 -0600

    Implementing an advanced image

--- app/src/main/java/com/example/jetpackcomposecatalogo/ui/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,6 +22,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -40,6 +42,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -69,7 +72,7 @@ class MainActivity : ComponentActivity() {
                         }
                         MyStateHoisting(myText) { myText = it }
                     } */
-                    MyImage()
+                    MyImageAdvanced()
                 }
             }
         }
@@ -91,8 +94,18 @@ fun DefaultPreview() {
 //        MyTextFieldOutlined()
 //        MyButton()
 //        MyDifferentButtons()
-        MyImage()
+//        MyImage()
+        MyImageAdvanced()
     }
+}
+
+@Composable
+fun MyImageAdvanced() {
+    Image(
+        painter = painterResource(R.drawable.ic_launcher_background),
+        contentDescription = "ejemplo",
+        modifier = Modifier.clip(CircleShape).border(5.dp, Color.Red, CircleShape)
+    )
 }
 
 @Composable
