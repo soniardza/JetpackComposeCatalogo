139d30341d35f4b281ae1ab70f3c1bf93e1a8625
“soniardza” <sonia.rdzalv@gmail.com>
2023-07-31 18:16:40 -0600

    Implementing an advance custom dialog and refactor some code

--- app/src/main/java/com/example/jetpackcomposecatalogo/Dialogs.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/Dialogs.kt
@@ -1,12 +1,17 @@
 package com.example.jetpackcomposecatalogo
 
 import android.util.Log
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -18,13 +23,94 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 
 const val EXAMPLE_TEXT = "Esto es un ejemplo"
+const val SHOW_DIALOG = "Mostrar diálogo"
 
+// An advanced custom dialog
+@Composable
+fun ShowMyCustomDialog() {
+    var show by remember {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { show = true }) {
+            Text(text = SHOW_DIALOG)
+        }
+        MyCustomDialog(
+            show = show,
+            onDismiss = { show = false }
+        )
+    }
+}
+
+@Composable
+fun MyCustomDialog(
+    show: Boolean,
+    onDismiss: () -> Unit,
+) {
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .background(Color.White)
+                    .padding(24.dp)
+                    .fillMaxWidth()
+            ) {
+                MyTitleDialog(text = "Set backup account")
+                AccountItem(email = "ejemplo1@gmail.com", drawable = R.drawable.avatar)
+                AccountItem(email = "ejemplo1@gmail.com", drawable = R.drawable.avatar)
+                AccountItem(email = "Add account", drawable = R.drawable.add)
+            }
+        }
+    }
+}
+
+@Composable
+fun MyTitleDialog(text: String) {
+    Text(
+        text = text,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(12.dp)
+    )
+}
+
+@Composable
+fun AccountItem(email: String, @DrawableRes drawable: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = drawable),
+            contentDescription = "Avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = email,
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+// A simple custom dialog
 @Composable
 fun ShowMySimpleCustomDialog() {
     var show by remember {
@@ -36,7 +122,7 @@ fun ShowMySimpleCustomDialog() {
         contentAlignment = Alignment.Center
     ) {
         Button(onClick = { show = true }) {
-            Text(text = "Mostrar diálogo")
+            Text(text = SHOW_DIALOG)
         }
         MySimpleCustomDialog(
             show = show,
@@ -72,6 +158,7 @@ fun MySimpleCustomDialog(
     }
 }
 
+// An alert dialog
 @Composable
 fun ShowMyAlertDialog() {
     var show by remember {
@@ -83,7 +170,7 @@ fun ShowMyAlertDialog() {
         contentAlignment = Alignment.Center
     ) {
         Button(onClick = { show = true }) {
-            Text(text = "Mostrar diálogo")
+            Text(text = SHOW_DIALOG)
         }
         MyAlertDialog(
             show = show,
